74168474d2bbbdb237fb5baed06d53d9fc6caab6
Rishabh Rawat <rishabh.rawat@smallcase.com>
2022-12-22 12:40:58 +0530

    refactor: update cloudformation yml file

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 echo "Code ApplicationStart event script ran."
 
-npm i
+# CodeDeploy does not take source files from what you specify in the files section of the appspec.yml
+# It picks the code revision from inside /opt/codedeploy-agent/
+
+# Move to the correct code location
+cd /deploy-nodejs-aws-cicd 
+
+sudo npm i
 
 npx pm2 start index.js --name=NodejsDeploymentStackApplication
